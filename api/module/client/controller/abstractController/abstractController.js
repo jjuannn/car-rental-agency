676bf882c1ed4f77b6fb1de51b8d8a97d7d66b94
juan <juancraft12yt@gmail.com>
2021-06-11 21:26:14 -0300

    delete unused files

--- api/module/client/controller/abstractController/abstractController.js
+++
@@ -1,9 +0,0 @@
-const AbstractControllerError = require('../error/abstractControllerError');
-
-module.exports = class AbstractController {
-  constructor() {
-    if (new.target === AbstractController) {
-      AbstractControllerError();
-    }
-  }
-};
