b3c038273be3b305933d1cbb06c2c349cec52f20
juan <juancraft12yt@gmail.com>
2021-07-04 22:30:37 -0300

    create car service for api calls

---
+++ client/src/services/car.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import {apiToEntity} from '../mappers/car';
+
+// Backend running at port 8080 by default.
+// Change this if you modify that.
+const BASE_URL = 'http://localhost:8080';
+
+const AXIOS_REQ = axios.create({
+  baseURL: BASE_URL
+});
+
+export function getCars() {
+  return AXIOS_REQ.get('/car/all')
+    .then(res => {
+      return res.data.map(car => {
+        return apiToEntity(car);
+      });
+    })
+    .then(rjson => {
+      return rjson;
+    });
+}
+
+export function addCar(data) {
+  return axios({
+    method: 'POST',
+    url: `${BASE_URL}/car/new`,
+    data,
+    headers: {'Content-Type': 'multipart/form-data'}
+  })
+    .then(res => {
+      return res.data;
+    })
+    .catch(e => {
+      throw new Error(e.response.data);
+    });
+}
