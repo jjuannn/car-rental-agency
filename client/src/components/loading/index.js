c3dfda70f869e6bc5a2d72fbd5189217d328a8d9
juan <juancraft12yt@gmail.com>
2021-07-04 22:31:45 -0300

    create loading component

---
+++ client/src/components/loading/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Box, Spinner} from '@chakra-ui/react';
+
+export default function Loading() {
+  return (
+    <Box display='flex' marginTop='30px' justifyContent='center'>
+      <Spinner thickness='4px' speed='0.65s' emptyColor='gray.200' color='teal.main' size='xl' />
+    </Box>
+  );
+}
