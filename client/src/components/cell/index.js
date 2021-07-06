bb6bab9044beec324b6927888830ae6158544cb4
juan <juancraft12yt@gmail.com>
2021-07-05 22:39:33 -0300

    implemented delete feature in the table cell's
    also, modify buttons and add a colorScheme instead of a plain color

--- client/src/components/cell/index.js
+++ client/src/components/cell/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {AiOutlineEye, AiOutlineEdit, AiOutlineDelete} from 'react-icons/ai';
 import {HStack, Button, Text, Tr, Td, Image} from '@chakra-ui/react';
+import {useHistory} from 'react-router-dom';
+import useCars from '../../hooks/useCars';
+export default function TableCell({images, year, model, brand, id}) {
+  const {deleteCarError, deleteCarLoading, deleteCarSuccess, deleteCar} = useCars();
+  const history = useHistory();
 
-export default function TableCell({images, year, model, brand}) {
+  useEffect(() => {
+    if (deleteCarSuccess) {
+      history.go(0);
+    }
+  }, [deleteCarSuccess]);
+
   return (
     <Tr>
       <Td>
@@ -15,13 +25,20 @@ export default function TableCell({images, year, model, brand}) {
       </Td>
       <Td isNumeric>
         <HStack justifyContent='flex-end'>
-          <Button>
+          <Button
+            isDisabled={deleteCarLoading}
+            colorScheme='red'
+            variant='outline'
+            onClick={() => {
+              deleteCar(id);
+            }}
+          >
             <AiOutlineDelete />
           </Button>
-          <Button>
+          <Button colorScheme='green' variant='outline'>
             <AiOutlineEye />
           </Button>
-          <Button>
+          <Button colorScheme='blue' variant='outline'>
             <AiOutlineEdit />
           </Button>
         </HStack>
