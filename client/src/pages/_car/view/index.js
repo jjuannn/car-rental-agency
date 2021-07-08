9e973f016149dd029963bd6c7a8279f9cc91b153
juan <juancraft12yt@gmail.com>
2021-07-07 23:14:59 -0300

    Create index.js

---
+++ client/src/pages/_car/view/index.js
@@ -0,0 +1,23 @@
+import React, {useEffect} from 'react';
+import {Flex, Box} from '@chakra-ui/react';
+import {useParams} from 'react-router-dom';
+import useCars from '../../../hooks/useCars';
+import CarDetailCard from '../../../components/detail/car';
+export default function CarDetail() {
+  const {id} = useParams();
+  const {getCar, carDetailData, carDetailError, carDetailLoading} = useCars();
+
+  useEffect(() => {
+    getCar(id);
+  }, []);
+
+  if (carDetailData) {
+    console.log(carDetailData);
+  }
+
+  return (
+    <Flex as='section' flex='1' padding='5' justifyContent='center'>
+      {carDetailData && <CarDetailCard {...carDetailData} />}
+    </Flex>
+  );
+}
